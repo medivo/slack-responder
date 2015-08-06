2ed20d69ade3941a5bd68472ae2f13ca6b0d6564
MrPowers <matthewkevinpowers@gmail.com>
2015-08-06 13:06:24 -0500

    Added the routes I forgot about

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   post "/slack/work", :to => "slack_commands#work"
+  post "/slack/retro", :to => "slack_commands#retro"
+  post "/slack/create_card", :to => "slack_commands#create_card"
 end
